e1df4a9074ef008223c423ae776fbae88f3bed42
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-05-03 12:25:00 +0530

    Update dice.py

--- userbot/modules/dice.py
+++ userbot/modules/dice.py
@@ -2,8 +2,8 @@
 
 
 from telethon.tl.types import InputMediaDice
-from emoji import *
 import emoji
+from emoji import *
 from userbot.events import register 
 from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP, bot, ALIVE_NAME
 
