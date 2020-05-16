33136807547b007556355b7043fc737832b628b2
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-05-16 11:17:36 +0530

    Update updater.py

--- userbot/modules/updater.py
+++ userbot/modules/updater.py
@@ -29,7 +29,7 @@ async def gen_chlog(repo, diff):
 
 
 async def is_off_br(br):
-    off_br = ['sql-extended', 'staging']
+    off_br = ['sql-extended']
     if br in off_br:
         return 1
     return
