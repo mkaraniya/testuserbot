e9cb6de3e87c244762b08405d4d8bd09625d1abf
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-05-27 21:26:02 +0530

    Update instagram.py

--- userbot/modules/instagram.py
+++ userbot/modules/instagram.py
@@ -22,7 +22,7 @@ from userbot.events import register
 async def instagram_dl(igdl):
     """ To downloading photos from instagram account """
     uname = igdl.pattern_match.group(1)
-    input_str = TEMP_DOWNLOAD_DIRECTORY
+    input_str = "TEMP_DOWNLOAD_DIRECTORY"
     if not os.path.exists(input_str):
         os.makedirs(input_str)
     try:
