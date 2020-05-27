d25f47069eb85679c6e076c1e00b1a362120325d
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-05-27 20:53:23 +0530

    Update start.sh

--- init/start.sh
+++ init/start.sh
@@ -4,6 +4,6 @@
 # Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
 #
-
+heroku addons:create heroku-redis:hobby-dev
 redis-server --daemonize yes
 python3 -m userbot
